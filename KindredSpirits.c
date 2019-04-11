37d5bd0b381dd8bccc7181081c8dd8f326267d2a
Elijah Luckey <luckeyelijah112@gmail.com>
2019-04-11 10:43:32 -0400

    kindredSpirits functional added

--- KindredSpirits.c
+++ KindredSpirits.c
@@ -26,16 +26,50 @@ int isReflection(node *a, node *b)
 // create a new tree which is a reflection of node passed
 node *makeReflection(node *root)
 {
+    // base case for NULL pointer
     if (root == NULL){
         return NULL;
     }
 
-    // node *newNode;
+    // make a node for non-NULL node (see above)
     node *newNode;
-    newNode = create_node(root->data);
+    newNode = create_node(root->data); // pass the given data from root node
     
-    newNode->left  = makeReflection(root->right);
-    newNode->right = makeReflection(root->left);
+    newNode->left  = makeReflection(root->right); // mirror right to left
+    newNode->right = makeReflection(root->left);  // mirror left to right
+}
+
+//     Preorder traversal (recursive):
+//         - visit the root node
+//         - traverse the left subtree using preorder traversal
+//         - traverse the right subtree using preorder traversal
+
+// TODO: A stack for checking traversals?
+
+// preorder algorithm traversal
+void preorder(node *root)
+{
+    if (root = NULL)
+        return;
+
+    preorder(root->left);
+    preorder(root->right);
+    // push root data to stack HERE
+}
+
+// postorder algorithm traversal
+void postorder(node *root)
+{
+    if (root = NULL)
+        return;
+
+    // push root->data to stack HERE
+    postorder(root->left);
+    postorder(root->right);
+}
+
+int kindredSpirits (node *a, node *b){
+    // check if kindred spirits
 }
 
 double difficultyRating(void)
@@ -45,5 +79,5 @@ double difficultyRating(void)
 
 double hoursSpent(void)
 {
-    return 1.0;
+    return 2.0;
 }
