006048b157a3e85bcb41657a03075dd26db6dfe2
Elijah Luckey <eluckey@Frederick.local>
2019-04-21 09:22:55 -0400

    debuggin

--- KindredSpirits.c
+++ KindredSpirits.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "KindredSpirits.h"
 
+typedef struct kindred_spirit
+{
+    int *pre_data;  // current value at PREorder traversal of node A
+    int *post_data; 
+}kindred_spirit;
+
 // passing references
 node *create_node(int data)
 {
@@ -45,61 +51,87 @@ node *makeReflection(node *root)
     
     newNode->left  = makeReflection(root->right); // mirror right to left
     newNode->right = makeReflection(root->left);  // mirror left to right
+
+    return newNode;
 }
 // TODO: A stack for checking traversals?
 
 // preorder algorithm traversal
-void preorder(node *root)
+int *preorder(node *root, int *pre_a, int counter)
 {
-    // if (root = NULL)
-    //     {return NULL;}
-
-    preorder(root->left);
-    preorder(root->right);
-    printf("%d ", root->data);
-    // push root data to stack HERE
+    if (root == NULL)
+        {return NULL;}
+    preorder(root->left, pre_a, counter++);
+    preorder(root->right, pre_a, counter++);
+    printf("[%d]=%d ",counter, root->data);
+    pre_a[counter] = root->data;
+    return pre_a;
 }
 
 // postorder algorithm traversal
-void postorder(node *root)
+int *postorder(node *root, int *post_a, int counter)
 {
-    // if (root = NULL)
-    //     {return NULL;}
+    if (root == NULL)
+        {return NULL;}
     
-    printf("%d ", root->data);
-    postorder(root->left);
-    postorder(root->right); 
+    printf("[%d]=%d ",counter, root->data);
+    post_a[counter] = root->data;
+    postorder(root->left, post_a, counter++);
+    postorder(root->right, post_a, counter++);
+
+    return post_a;
 }
-
-typedef struct kindred_spirit
-{
-    int pre_data;  // current value at PREorder traversal of node A
-    int post_data; // current value at POSTorder traversal of node B
-    //ony used in 
-}kindred_spirit;
-
-int pre_post_traversal(node *a, node *b){
-    // return 0 -> a&b same
-    // return 1 -> a&b not same
-    return 0;
-}
-
 
 // a KindredSpirit is when a trees preorder trav is the same
 // as the postoreder traversal
 int kindredSpirits(node *a, node *b){
 
-    printf("POSTORDER A: \n");
-    postorder(a);
+    int counter = 0;
+    int *pre_a;
+    int *post_a;
 
-    printf("PREORDER B: \n");
-    preorder(b);
+    post_a = postorder(a, post_a, counter);
+    printf("\n");
+    pre_a = preorder(b, pre_a, counter);
+    printf("\n");
 
-    printf("Kindred Spirit Test: \n");
-    printf("%d ", pre_post_traversal(a, b));
+    printf("PRE-Order: ");
+    int i;
+    for(i = 0; i < ((sizeof(pre_a))/(sizeof(int))); i++){
+        printf("%d ", pre_a[i]);
+    } printf("\n");
+
+    printf("POST-Order: ");
+    for(i = 0; i < ((sizeof(post_a))/(sizeof(int))); i++){
+        printf("%d ", post_a[i]);
+    } printf("\n");
 
     return 0;
 }
+
+// int main(int argc, char const *argv[])
+// {
+//     node *root1, *root2;
+
+// 	// These are the trees from Figure 9 in the assignment PDF, used for testing
+// 	// the kindredSpirits function.
+// 	root1 = create_node(23);
+// 	root1->left = create_node(12);
+// 	root1->left->left = create_node(5);
+// 	root1->left->right = create_node(18);
+// 	root1->right = create_node(71);
+// 	root1->right->right = create_node(56);
+
+// 	root2 = create_node(56);
+// 	root2->left = create_node(23);
+// 	root2->right = create_node(71);
+// 	root2->right->left = create_node(5);
+// 	root2->right->left->right = create_node(12);
+// 	root2->right->right = create_node(18);
+
+//     return kindredSpirits(root1, root2);
+// }
+
 
 double difficultyRating(void)
 {
